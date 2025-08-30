9ef6df3ac13c666de316f6b4679e34b060ec9228
Lray143 <Lray.gulferic@gmail.com>
2025-08-30 18:51:16 +0800

    draft

---
+++ Login_Page.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Login_Page : Form
+    {
+        public Login_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
