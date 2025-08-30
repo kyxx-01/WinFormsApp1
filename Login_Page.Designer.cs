50bf397a0dd751018ed4a7a20b927faf05cc11bd
Lray143 <Lray.gulferic@gmail.com>
2025-08-30 21:34:18 +0800

    darft login

--- Login_Page.Designer.cs
+++ Login_Page.Designer.cs
@@ -30,23 +30,26 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_Page));
             AdminPB = new PictureBox();
-            LoginPanel = new Panel();
-            loginicopb = new PictureBox();
             label1 = new Label();
             passwordpanel = new Panel();
             passwordicopb = new PictureBox();
             registerbutton = new Button();
             loginbutton = new Button();
+            loginicopb = new PictureBox();
+            LoginPanel = new Panel();
+            passwordTxt = new TextBox();
+            loginTxt = new TextBox();
             ((System.ComponentModel.ISupportInitialize)AdminPB).BeginInit();
-            LoginPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)loginicopb).BeginInit();
             passwordpanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)passwordicopb).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)loginicopb).BeginInit();
+            LoginPanel.SuspendLayout();
             SuspendLayout();
             // 
             // AdminPB
             // 
             AdminPB.BackColor = Color.Transparent;
+            AdminPB.BackgroundImage = (Image)resources.GetObject("AdminPB.BackgroundImage");
             AdminPB.Image = (Image)resources.GetObject("AdminPB.Image");
             AdminPB.Location = new Point(136, 65);
             AdminPB.Name = "AdminPB";
@@ -54,26 +57,6 @@
             AdminPB.SizeMode = PictureBoxSizeMode.StretchImage;
             AdminPB.TabIndex = 0;
             AdminPB.TabStop = false;
-            // 
-            // LoginPanel
-            // 
-            LoginPanel.BackColor = Color.White;
-            LoginPanel.Controls.Add(loginicopb);
-            LoginPanel.Location = new Point(67, 309);
-            LoginPanel.Name = "LoginPanel";
-            LoginPanel.Size = new Size(323, 58);
-            LoginPanel.TabIndex = 1;
-            LoginPanel.Paint += LoginPanel_Paint;
-            // 
-            // loginicopb
-            // 
-            loginicopb.Image = (Image)resources.GetObject("loginicopb.Image");
-            loginicopb.Location = new Point(11, 10);
-            loginicopb.Name = "loginicopb";
-            loginicopb.Size = new Size(40, 40);
-            loginicopb.SizeMode = PictureBoxSizeMode.StretchImage;
-            loginicopb.TabIndex = 3;
-            loginicopb.TabStop = false;
             // 
             // label1
             // 
@@ -89,6 +72,7 @@
             // passwordpanel
             // 
             passwordpanel.BackColor = Color.White;
+            passwordpanel.Controls.Add(passwordTxt);
             passwordpanel.Controls.Add(passwordicopb);
             passwordpanel.Location = new Point(67, 384);
             passwordpanel.Name = "passwordpanel";
@@ -125,6 +109,43 @@
             loginbutton.Text = "Login";
             loginbutton.UseVisualStyleBackColor = false;
             // 
+            // loginicopb
+            // 
+            loginicopb.Image = (Image)resources.GetObject("loginicopb.Image");
+            loginicopb.Location = new Point(11, 10);
+            loginicopb.Name = "loginicopb";
+            loginicopb.Size = new Size(40, 40);
+            loginicopb.SizeMode = PictureBoxSizeMode.StretchImage;
+            loginicopb.TabIndex = 3;
+            loginicopb.TabStop = false;
+            // 
+            // LoginPanel
+            // 
+            LoginPanel.BackColor = Color.White;
+            LoginPanel.Controls.Add(loginTxt);
+            LoginPanel.Controls.Add(loginicopb);
+            LoginPanel.Location = new Point(67, 309);
+            LoginPanel.Name = "LoginPanel";
+            LoginPanel.Size = new Size(323, 58);
+            LoginPanel.TabIndex = 1;
+            LoginPanel.Paint += LoginPanel_Paint;
+            // 
+            // passwordTxt
+            // 
+            passwordTxt.Location = new Point(57, 17);
+            passwordTxt.Multiline = true;
+            passwordTxt.Name = "passwordTxt";
+            passwordTxt.Size = new Size(249, 28);
+            passwordTxt.TabIndex = 7;
+            // 
+            // loginTxt
+            // 
+            loginTxt.Location = new Point(57, 15);
+            loginTxt.Multiline = true;
+            loginTxt.Name = "loginTxt";
+            loginTxt.Size = new Size(249, 28);
+            loginTxt.TabIndex = 8;
+            // 
             // Login_Page
             // 
             AutoScaleDimensions = new SizeF(12F, 27F);
@@ -150,10 +171,12 @@
             Text = "Login_Page";
             Load += Login_Page_Load;
             ((System.ComponentModel.ISupportInitialize)AdminPB).EndInit();
-            LoginPanel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)loginicopb).EndInit();
             passwordpanel.ResumeLayout(false);
+            passwordpanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)passwordicopb).EndInit();
+            ((System.ComponentModel.ISupportInitialize)loginicopb).EndInit();
+            LoginPanel.ResumeLayout(false);
+            LoginPanel.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -161,12 +184,14 @@
         #endregion
 
         private PictureBox AdminPB;
-        private Panel LoginPanel;
-        private PictureBox loginicopb;
         private Label label1;
         private Panel passwordpanel;
         private PictureBox passwordicopb;
         private Button registerbutton;
         private Button loginbutton;
+        private TextBox passwordTxt;
+        private PictureBox loginicopb;
+        private Panel LoginPanel;
+        private TextBox loginTxt;
     }
 }
